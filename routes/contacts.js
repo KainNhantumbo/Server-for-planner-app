9ae4d75f047847c876d78900af0224fb649b7cca
Kain Nhantumbo <nhantumbok@gmail.com>
2022-04-11 08:01:00 +0200

    feat: support for create new contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Contact = require('../models/contact-model');
 
 router
 	.route('/')
 	.get((req, res) => {
 		res.status(200).json({ name: 'Lara Crawller', number: 782398374 });
 	})
+	.post(async (req, res) => {
+		try {
+			const newContact = await Contact.create(req.body);
+			res.status(201).json(newContact);
+		} catch (err) {
+			console.log(err);
+		}
+	});
 
 module.exports = router;
